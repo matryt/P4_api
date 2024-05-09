789ee14c0f3e74a7d91d36e77099a35f3bb8cc21
matryt <64699519+matryt@users.noreply.github.com>
2024-05-09 12:58:46 +0200

    Routes users (#3)
    * Config routes user
    
    * Apply suggestions from Sourcery
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- models/Game.js
+++ models/Game.js
@@ -5,7 +5,8 @@ const GameSchema = new mongoose.Schema({
     players: {type: Array, required: true},
     pieces: {type: Array, required: true},
     isFinished: {type: Boolean, required: true, default: false},
-    isFirstTurn: {type: Boolean, required: true, default: true}
+    isFirstTurn: {type: Boolean, required: true, default: true},
+    modifs: {type: Number, required: false, default: 0}
 })
 
 module.exports = mongoose.model("Game", GameSchema)
